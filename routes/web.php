362b92997d208b259b78fbd7c2258dbfb2d246a5
Matt Stauffer <matt@tighten.co>
2020-03-10 11:26:30 -0400

    Drop auth, write readme

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
