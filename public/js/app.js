95f77615406e2b2df1f7b7dd22cdaf7cab8b3a9a
Vishnu Padmanabhan <vishnu@elemento.local>
2020-03-16 12:19:02 +0530

    added smooth scroll

--- public/js/app.js
+++ public/js/app.js
@@ -19346,8 +19346,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/mattstauffer/Sites/tallstack/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/mattstauffer/Sites/tallstack/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/vishnu/Code/Laravel/tallstack/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/vishnu/Code/Laravel/tallstack/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
